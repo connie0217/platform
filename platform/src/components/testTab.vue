fa0c55b6c5394d0de704a4681b1773ad16b15e8d
connie0217 <415178290@qq.com>
2017-11-07 12:53:48 +0800

    add-router

---
+++ platform/src/components/testTab.vue
@@ -0,0 +1,69 @@
+<template id="">
+  <div class="tab-wrap">
+    <div class="tab-tit">
+      <a href="#" v-for="(item,index) in tabTit" @click="changeTab(index)" :class="{'cur':isTab==index}">{{item}}</a>
+    
+      
+    </div>
+    <div class="tab-con">
+      <div v-for="(item,index) in tabCon" v-show="isTab==index">{{item}}</div>
+      
+    </div>
+  </div>
+</template>
+<script type="text/javascript">
+// 点第几个，第几个是选中状态，使用变量来设置tab的选中状态
+export default{
+  data(){
+    return {
+      isTab:0,//初始值
+      tabTit:[
+        "HTML","CSS","JS","SASS","ES6"
+      ],
+      tabCon:[
+        "hello html","hello css","hello js","hello sass","hello es6"
+      ]
+      
+    }
+  },
+  methods:{
+    changeTab(index){
+      this.isTab=index
+    }
+  }
+}
+  
+</script>
+<style>
+body{
+  font-family:"Microsoft YaHei";
+}
+.tab-wrap{
+  width: 600px;
+  margin: 0 auto;
+}
+.tab-tit{
+  font-size: 0;
+  width: 600px;
+}
+.tab-tit a{
+  display: inline-block;
+  height: 40px;
+  line-height: 40px;
+  font-size: 16px;
+  width: 20%;
+  text-align: center;
+  background: #ccc;
+  color: #333;
+  text-decoration: none;
+}
+.tab-tit .cur{
+  background: #09f;
+  color: #fff;
+}
+.tab-con div{
+  border: 1px solid #ccc;
+  height: 400px;
+  padding-top: 20px;
+}
+</style>
--- platform/src/main.js
+++ platform/src/main.js
@@ -2,11 +2,37 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import Layout from './components/Layout'
+import Tab from './components/testTab'
+import Car from './components/buyCar'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+import index from './pages/index'
 
+Vue.use(VueRouter);
+Vue.use(VueResource);
+const router =new VueRouter({
+  mode:'history',
+  routes:[
+    {
+      path:'/',
+      component:index
+    },
+    {
+      path:'/tab',
+      component:Tab
+    },
+    {
+      path:'/car',
+      component:Car
+    },
+    
+  ]
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   template: '<Layout/>',
   components: { Layout }
 })
